e9266d9387dd6b57186807880cfe7d82967b6ea6
Heat Koemnak <heatkimnak@gmail.com>
2025-08-19 13:11:35 +0700

    .

--- tina/config.js
+++ tina/config.js
@@ -11,6 +11,9 @@ export default defineConfig({
   branch,
 
   // Get this from tina.io
+  // clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || "",
+  // // Get this from tina.io
+  // token: process.env.TINA_TOKEN || "",
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || "b439f5e7-f9cc-46ec-b156-b1e9e3f3f9c1",
   // Get this from tina.io
   token: process.env.TINA_TOKEN || "d8431503daa0e2de1b7ec1df2c80250af62ebc81",
@@ -27,31 +30,34 @@ export default defineConfig({
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
-    collections: [
-      {
-        name: "post",
-        label: "Posts",
-        path: "content/posts",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-        ui: {
-          // This is an DEMO router. You can remove this to fit your site
-          router: ({ document }) => `/demo/blog/${document._sys.filename}`,
+  collections: [
+    {
+      name: "page",
+      label: "Pages",
+      path: "content/pages",
+      format: "md", // or "mdx" depending on your content files
+      fields: [
+        {
+          type: "string",
+          name: "title",
+          label: "Title",
+          isTitle: true,
+          required: true,
+        },
+        {
+          type: "rich-text",
+          name: "body",
+          label: "Body",
+          isBody: true,
+        },
+      ],
+      ui: {
+        // Example custom router
+        router: ({ document }) => {
+          return `/pages/${document._sys.filename}`;
         },
       },
-    ],
-  },
+    },
+  ],
+}
 });
